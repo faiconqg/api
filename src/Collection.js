1c21fc4136968f2c3e97f8a6cef025abddb460d9
Thiago Lins <thiagolins@faicon.com>
2018-03-02 18:25:34 -0300

    Incorporate mobx

--- src/Collection.js
+++ src/Collection.js
@@ -2,4 +2,5 @@ import { Collection as BaseCollection } from 'mobx-rest'
 
 export default class Collection extends BaseCollection {
   where = filterObject => this.fetch({ data: { filter: { where: filterObject } } })
+  busy = () => this.isRequest('fetching')
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { default as Collection } from './Collection'
 export { default as Model } from './Model'
 export { default as apiSetup } from './Setup'
-export { observer } from 'mobx-react'
+export { Provider, inject, observer } from 'mobx-react'
+export { observable, action, reaction } from 'mobx'
